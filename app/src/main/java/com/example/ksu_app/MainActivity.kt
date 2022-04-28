e4890c5575c1cf3645f893d09669450564159ebc
mestnyanarxist <rodgdestwo@yandex.ru>
2022-04-29 02:08:39 +0300

    1

---
+++ app/src/main/java/com/example/ksu_app/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.ksu_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ksu_app.databinding.ActivityMainBinding
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.holder.BadgeStyle
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+}
